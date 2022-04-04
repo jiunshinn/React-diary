8c3c928a0d95302bd578b3418cf1a5540335e565
jiun-dev <jiun.dev@gmail.com>
2022-04-05 05:51:27 +0900

    react get data and render

--- src/App.js
+++ src/App.js
@@ -44,7 +44,16 @@ function App() {
 
   const getData = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
-    console.log(res);
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_at: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
   }
 
   // Lifecycle : 컴포넌트 랜더링 시점, 탄생시점
